64814100e5862d081ee7be2b34178ee10ef9a970
westefns-souza <westefns@outlook.com.br>
2024-01-23 22:17:51 -0300

    Insert dos perfis no banco de dados

---
+++ EDUCACOOPERN/Data/Migrations/20240123233827_AdicionarPerfisDeAcesso.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EDUCACOOPERN.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarPerfisDeAcesso : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { "1", "COORDENADOR", "Coordenador", Guid.NewGuid().ToString() });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { "2", "PROFESSOR", "Professor", Guid.NewGuid().ToString() });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[] { "3", "COOPERADO", "Cooperado", Guid.NewGuid().ToString() });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles", 
+                keyColumn: "Id", 
+                keyValues: new[] { "1", "2", "3" });
+        }
+    }
+}
