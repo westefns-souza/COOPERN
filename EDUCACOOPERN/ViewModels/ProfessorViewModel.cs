1d4386bcda9af2438db6a357fd9b1855cacd6a7e
westefns-souza <westefns@outlook.com.br>
2024-01-28 19:16:03 -0300

    Cadastro de professores

---
+++ EDUCACOOPERN/ViewModels/ProfessorViewModel.cs
@@ -0,0 +1,29 @@
+﻿using EDUCACOOPERN.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EDUCACOOPERN.ViewModels;
+
+public class ProfessorViewModel
+{
+    public string? Id { get; set; }
+
+    [Display(Name = "Nome completo")]
+    [Required(ErrorMessage = "O {0} é obrigatório!")]
+    public string Nome { get; set; }
+
+    [Display(Name = "E-mail")]
+    [Required(ErrorMessage = "O {0} é obrigatório!")]
+    [EmailAddress(ErrorMessage = "O {0} deve ser válido!")]
+    public string Email { get; set; }
+
+    [Display(Name = "Celular")]
+    [Required(ErrorMessage = "O {0} é obrigatório!")]
+    public string Celular { get; set; }
+
+    [Display(Name = "Áreas de atuação")]
+    [Required(ErrorMessage = "O {0} é obrigatório!")]
+    public List<AreaAtuacao> AreasAtuacao { get; set; }
+
+    [Display(Name = "Ativo")]
+    public bool Ativo { get; set; }
+}
