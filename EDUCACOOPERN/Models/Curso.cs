2a544b7ef02534358f1fdcd51bb10ed64cb348ae
westefns-souza <westefns@outlook.com.br>
2024-04-22 22:34:59 -0300

    Aumentando o tamanho do campo dos cursos

--- EDUCACOOPERN/Models/Curso.cs
+++ EDUCACOOPERN/Models/Curso.cs
@@ -8,7 +8,7 @@ public class Curso
     public int Id { get; set; }
 
     [Required(ErrorMessage = "O {0} do curso é obrigatória!")]
-    [StringLength(24, MinimumLength = 3, ErrorMessage = "O {0} do curso deve ter entre 3 e 24 caracteres!")]
+    [StringLength(256, MinimumLength = 3, ErrorMessage = "O {0} do curso deve ter entre 3 e 256 caracteres!")]
     public string? Nome { get; set; }
 
     [Display(Name = "Descrição")]
