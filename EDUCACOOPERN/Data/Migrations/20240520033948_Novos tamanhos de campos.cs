46df5f4f367a9608c9a78bdd4f6f6b38bf2557f1
westefns-souza <westefns@outlook.com.br>
2024-05-20 00:40:33 -0300

    vovass migrações

---
+++ EDUCACOOPERN/Data/Migrations/20240520033948_Novos tamanhos de campos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EDUCACOOPERN.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Novostamanhosdecampos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
