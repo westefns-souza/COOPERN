4e6176157e3cd5b03a4b70df4b33639d40822a70
westefns-souza <westefns@outlook.com.br>
2024-09-12 22:13:15 -0300

    Permitindo mais caracteres no PDI

--- EDUCACOOPERN/Models/PDI.cs
+++ EDUCACOOPERN/Models/PDI.cs
@@ -1,5 +1,4 @@
-﻿using EDUCACOOPERN.Data.Migrations;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace EDUCACOOPERN.Models;
 
@@ -9,7 +8,7 @@ public class PDI
     public int Id { get; set; }
 
     [Required(ErrorMessage = "O {0} do PDI é obrigatória!")]
-    [StringLength(24, MinimumLength = 3, ErrorMessage = "O {0} do PDI deve ter entre 3 e 24 caracteres!")]
+    [StringLength(256, MinimumLength = 3, ErrorMessage = "O {0} do PDI deve ter entre 3 e 256 caracteres!")]
     public string? Nome { get; set; }
 
     [Display(Name = "Ativo")]
--- EDUCACOOPERN/ViewModels/PDIViewModel.cs
+++ EDUCACOOPERN/ViewModels/PDIViewModel.cs
@@ -9,7 +9,7 @@ public class PDIViewModel
     public int Id { get; set; }
 
     [Required(ErrorMessage = "O {0} do PDI é obrigatória!")]
-    [StringLength(24, MinimumLength = 3, ErrorMessage = "O {0} do PDI deve ter entre 3 e 24 caracteres!")]
+    [StringLength(256, MinimumLength = 3, ErrorMessage = "O {0} do PDI deve ter entre 3 e 256 caracteres!")]
     public string? Nome { get; set; }
 
     [Display(Name = "Ativo")]
