05f3423f5d0a07408aecefedef9e09d18bb46a23
westefns-souza <westefns@outlook.com.br>
2024-09-17 22:40:05 -0300

    Correçao do tamanho do campo de nome dos cursos do pdi

--- EDUCACOOPERN/ViewModels/PDICursoViewModel.cs
+++ EDUCACOOPERN/ViewModels/PDICursoViewModel.cs
@@ -8,6 +8,6 @@ public class PDICursoViewModel
     public int Id { get; set; }
 
     [Required(ErrorMessage = "O {0} do curso é obrigatória!")]
-    [StringLength(24, MinimumLength = 3, ErrorMessage = "O {0} do curso deve ter entre 3 e 24 caracteres!")]
+    [StringLength(256, MinimumLength = 3, ErrorMessage = "O {0} do curso deve ter entre 3 e 256 caracteres!")]
     public string? Nome { get; set; }
 }
