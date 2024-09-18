9c88dbd34c8fde159369b090091a9b9ea8ae8db3
westefns-souza <westefns@outlook.com.br>
2024-09-17 22:22:58 -0300

    Novos tamanhos de campos

---
+++ EDUCACOOPERN/Data/Migrations/20240918012012_tamanho_campos.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EDUCACOOPERN.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class tamanho_campos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "PDIs",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(24)",
+                oldMaxLength: 24);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "PDIs",
+                type: "character varying(24)",
+                maxLength: 24,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+        }
+    }
+}
