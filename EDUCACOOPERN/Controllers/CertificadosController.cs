b39546d5e92d1315e62d471b963803e734a77d2a
westefns-souza <westefns@outlook.com.br>
2024-11-24 23:41:58 -0300

    primeiro modelo dos certificados

--- EDUCACOOPERN/Controllers/CertificadosController.cs
+++ EDUCACOOPERN/Controllers/CertificadosController.cs
@@ -3,16 +3,22 @@ using EDUCACOOPERN.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace EDUCACOOPERN.Controllers;
 
 public class CertificadosController : Controller
 {
     private readonly ApplicationDbContext _context;
+    private readonly IWebHostEnvironment _env;
 
-    public CertificadosController(ApplicationDbContext context)
+    public CertificadosController(ApplicationDbContext context, IWebHostEnvironment env)
     {
         _context = context;
+        _env = env;
     }
 
     // GET: Certificados
@@ -36,11 +42,9 @@ public class CertificadosController : Controller
             return NotFound();
         }
 
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
         var certificado = await _context.Certificado
             .Include(c => c.Usuario)
-            .FirstOrDefaultAsync(m => m.Id == id && m.UsuarioId.Equals(userId));
+            .FirstOrDefaultAsync(m => m.Id == id);
 
         if (certificado == null)
         {
@@ -88,8 +92,6 @@ public class CertificadosController : Controller
         return RedirectToAction(nameof(Index));
     }
 
-    
-
     // GET: Certificados/Delete/5
     public async Task<IActionResult> Delete(int? id)
     {
@@ -122,6 +124,59 @@ public class CertificadosController : Controller
 
         await _context.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult GerarCertificado(string id, string nome, string curso)
+    {
+        nome = nome.ToUpper();
+        curso = curso.ToUpper();
+        
+        var certificadoExisteste = _context.Certificado
+            .Where(x => x.UsuarioId.Equals(id) && x.Descricao.Equals(curso))
+            .FirstOrDefault();
+
+        if (certificadoExisteste != null)
+        {
+            return RedirectToAction("Details", new { id = certificadoExisteste.Id });
+        }
+
+        var nomeSaida = id + DateTime.Now.ToString("yyyyMMddhhmmss") + ".pdf";
+
+        var caminhoPdfTemplate = Path.Combine(_env.WebRootPath, "pdf", "modelo_certificado.pdf");
+        var caminhoPdfSaida = Path.Combine(_env.WebRootPath, "pdf", nomeSaida);
+
+        using var reader = new PdfReader(caminhoPdfTemplate);
+        using var stream = new FileStream(caminhoPdfSaida, FileMode.Create);
+        using var stamper = new PdfStamper(reader, stream);
+
+        var bf = BaseFont.CreateFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+        var cb = stamper.GetOverContent(1); // Assumindo que o nome deve ser inserido na primeira página
+
+        cb.BeginText();
+        cb.SetFontAndSize(bf, 24);
+
+        cb.ShowTextAligned(Element.ALIGN_CENTER, curso, 420, 300, 0);
+        cb.ShowTextAligned(Element.ALIGN_CENTER, nome, 420, 250, 0);
+
+        cb.EndText();
+        stamper.Close();
+
+        var bytes = System.IO.File.ReadAllBytes(caminhoPdfSaida);
+
+        var certificado = new Certificado
+        {
+            Arquivo = Convert.ToBase64String(bytes),
+            UsuarioId = id,
+            Descricao = curso,
+            Extencao = "pdf"
+        };
+
+        _context.Add(certificado);
+        _context.SaveChanges();
+        
+        System.IO.File.Delete(caminhoPdfSaida);
+
+        return RedirectToAction("Details", new { id = certificado.Id});
     }
 
     private bool CertificadoExists(int id)
