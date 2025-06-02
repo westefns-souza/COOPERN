ff8eb9980ec4590c432e2f5c2e454454d2d23b96
westefns-souza <westefns@outlook.com.br>
2025-06-02 19:54:56 -0300

    remoção da nota pré-teste e melhoria no filtro das aulas

--- EDUCACOOPERN/Controllers/AulasController.cs
+++ EDUCACOOPERN/Controllers/AulasController.cs
@@ -132,11 +132,12 @@ public class AulasController : Controller
             }
             else if (matricula.Compareceu && matricula.NotaPos <= 6)
             {
+
                 matricula.Status = EStatusMatricula.Reprovado;
             }
             else
             {
-                matricula.NotaPre = 0;
+                //matricula.NotaPre = 0;
                 matricula.NotaPos = 0;
                 matricula.Status = EStatusMatricula.Reprovado;
             }
